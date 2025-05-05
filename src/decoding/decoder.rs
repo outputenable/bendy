outputenable/bendy

commit 399761c75675e0c696703db48078083ee08ba527

Optimized decoding

src/decoding/decoder.rs
@@ -66,21 +66,15 @@ impl<'ser> Decoder<'ser> {
 
         let mut curpos = self.offset;
         let mut state = State::Start;
-
         let mut success = false;
         while curpos < self.source.len() {
             let c = self.source[curpos] as char;
-            match state {
-                State::Start => {
-                    if c == '-' {
-                        state = State::Sign;
-                    } else if c == '0' {
-                        state = State::Zero;
-                    } else if ('1'..='9').contains(&c) {
-                        state = State::Digits;
-                    } else {
-                        return Err(StructureError::unexpected("'-' or '0'..'9'", c, curpos));
-                    }
+            state = match state {
+                State::Start => match c {
+                    '-' => State::Sign,
+                    '0' => State::Zero,
+                    '0'..='9' => State::Digits,
+                    _ => return Err(StructureError::unexpected("'-' or '0'..'9'", c, curpos)),
                 },
                 State::Zero => {
                     if c == expected_terminator {
@@ -94,28 +88,25 @@ impl<'ser> Decoder<'ser> {
                         ));
                     }
                 },
-                State::Sign => {
-                    if ('1'..='9').contains(&c) {
-                        state = State::Digits;
-                    } else {
-                        return Err(StructureError::unexpected("'1'..'9'", c, curpos));
-                    }
+                State::Sign => match c {
+                    '1'..='9' => State::Digits,
+                    _ => return Err(StructureError::unexpected("'1'..'9'", c, curpos)),
                 },
-                State::Digits => {
-                    if ('0'..='9').contains(&c) {
-                        // do nothing, this is ok
-                    } else if c == expected_terminator {
+                State::Digits => match c {
+                    '0'..='9' => state,
+                    x if x == expected_terminator => {
                         success = true;
                         break;
-                    } else {
+                    },
+                    _ => {
                         return Err(StructureError::unexpected(
                             &format!("{:?} or '0'..'9'", expected_terminator),
                             c,
                             curpos,
-                        ));
-                    }
+                        ))
+                    },
                 },
-            }
+            };
             curpos += 1;
         }
 
@@ -123,14 +114,14 @@ impl<'ser> Decoder<'ser> {
             return Err(StructureError::UnexpectedEof);
         }
 
-        let slice = &self.source[self.offset..curpos];
+        #[cfg(debug)]
+        let ival = istr::from_utf8(&self.source[self.offset..curpos])
+            .expect("We've already examined every byte in the string");
+
+        #[cfg(not(debug))]
+        let ival = // Avoid a second UTF-8 check here
+            unsafe { str::from_utf8_unchecked(&self.source[self.offset..curpos]) };
         self.offset = curpos + 1;
-        let ival = if cfg!(debug) {
-            str::from_utf8(slice).expect("We've already examined every byte in the string")
-        } else {
-            // Avoid a second UTF-8 check here
-            unsafe { str::from_utf8_unchecked(slice) }
-        };
 
         Ok(ival)
     }
@@ -141,9 +132,8 @@ impl<'ser> Decoder<'ser> {
             'l' => Token::List,
             'd' => Token::Dict,
             'i' => Token::Num(self.take_int('e')?),
-            c if ('0'..='9').contains(&c) => {
+            '0'..='9' => {
                 self.offset -= 1;
-
                 let curpos = self.offset;
                 let ival = self.take_int(':')?;
                 let len: usize = str::parse(ival).map_err(|_| StructureError::SyntaxError {
@@ -176,7 +166,6 @@ impl<'ser> Decoder<'ser> {
 
         let tok_result = self.raw_next_token();
         let tok = self.state.latch_err(tok_result)?;
-
         self.state.observe_token(&tok)?;
         Ok(Some(tok))
     }

src/state_tracker/state.rs
@@ -26,7 +26,7 @@ pub struct StateTracker<S: AsRef<[u8]>, E = StructureError> {
 impl<S: AsRef<[u8]>, E> Default for StateTracker<S, E> {
     fn default() -> Self {
         StateTracker {
-            state: Vec::new(),
+            state: Vec::with_capacity(2048),
             max_depth: 2048,
         }
     }
@@ -66,77 +66,66 @@ where
         S: From<&'a [u8]>,
     {
         use self::{State::*, Token::*};
-
-        match (self.state.pop(), *token) {
+        let last_index = self.state.len().max(1) - 1;
+        let actual_len = self.state.len();
+        match (self.state.last_mut(), *token) {
             (None, End) => {
                 return self.latch_err(Err(E::from(StructureError::invalid_state(
                     "End not allowed at top level",
                 ))));
             },
-            (Some(Seq), End) => {},
-            (Some(MapKey(_)), End) => {},
+            (Some(Seq), End) | (Some(MapKey(_)), End) => {
+                self.state.pop();
+            },
             (Some(MapKey(None)), String(label)) => {
-                self.state.push(MapValue(S::from(label)));
+                self.state[last_index] = MapValue(S::from(label)); //TODO: looks similar!
             },
-            (Some(MapKey(Some(oldlabel))), String(label)) => {
-                if oldlabel.as_ref() >= label {
-                    return self.latch_err(Err(E::from(StructureError::UnsortedKeys)));
-                }
-                self.state.push(MapValue(S::from(label)));
+            (Some(MapKey(Some(oldlabel))), String(label)) if oldlabel.as_ref() >= label => {
+                self.state.pop();
+                return self.latch_err(Err(E::from(StructureError::UnsortedKeys)));
             },
-            (Some(oldstate @ MapKey(_)), _tok) => {
-                self.state.push(oldstate);
+            (Some(MapKey(Some(_oldlabel))), String(label)) => {
+                self.state[last_index] = MapValue(S::from(label));
+            },
+            (Some(_oldstate @ MapKey(_)), _tok) => {
+                self.state.pop();
                 return self.latch_err(Err(E::from(StructureError::invalid_state(
                     "Map keys must be strings",
                 ))));
             },
-            (Some(MapValue(label)), List) => {
-                self.state.push(MapKey(Some(label)));
+            (Some(MapValue(label)), List) | (Some(MapValue(label)), Dict) => {
+                let dummy: &[u8] = &[1];
+                self.state[last_index] = MapKey(Some(std::mem::replace(label, dummy.into())));
                 if self.state.len() >= self.max_depth {
                     return self.latch_err(Err(E::from(StructureError::NestingTooDeep)));
                 }
-                self.state.push(Seq);
+                self.state
+                    .push(if token == &List { Seq } else { MapKey(None) });
             },
-            (Some(MapValue(label)), Dict) => {
-                self.state.push(MapKey(Some(label)));
-                if self.state.len() >= self.max_depth {
-                    return self.latch_err(Err(E::from(StructureError::NestingTooDeep)));
-                }
-                self.state.push(MapKey(None));
-            },
-            (Some(oldstate @ MapValue(_)), End) => {
-                self.state.push(oldstate);
+            (Some(_oldstate @ MapValue(_)), End) => {
+                self.state.pop();
                 return self.latch_err(Err(E::from(StructureError::invalid_state(
                     "Missing map value",
                 ))));
             },
             (Some(MapValue(label)), _) => {
-                self.state.push(MapKey(Some(label)));
+                let dummy: &[u8] = &[1];
+                self.state[last_index] = MapKey(Some(std::mem::replace(label, dummy.into())));
             },
-            (oldstate, List) => {
-                if let Some(oldstate) = oldstate {
-                    self.state.push(oldstate);
+            (oldstate, List) | (oldstate, Dict) => {
+                if oldstate.is_none() && 0 < self.state.len() {
+                    self.state.pop();
                 }
                 if self.state.len() >= self.max_depth {
                     return self.latch_err(Err(E::from(StructureError::NestingTooDeep)));
                 }
-                self.state.push(Seq);
+                self.state
+                    .push(if token == &List { Seq } else { MapKey(None) });
             },
-            (oldstate, Dict) => {
-                if let Some(oldstate) = oldstate {
-                    self.state.push(oldstate);
-                }
-
-                if self.state.len() >= self.max_depth {
-                    return self.latch_err(Err(E::from(StructureError::NestingTooDeep)));
-                }
-                self.state.push(MapKey(None));
+            (oldstate, _) if oldstate.is_none() && 0 < actual_len => {
+                self.state.pop();
             },
-            (oldstate, _) => {
-                if let Some(oldstate) = oldstate {
-                    self.state.push(oldstate);
-                }
-            },
+            _ => {},
         }
         Ok(())
     }

commit 9af8d1dd818518c4ba9d53b774b8877ab60d2741

Fix typo in cfg(debug) code
Since a `cfg` condition name `debug` doesn't exist I guess the idea is
to manually enable the debug code with something like RUSTFLAGS='--cfg
debug'?  Anyway, apparently the disabled code isn't checked
automatically and a typo (introduced in
399761c75675e0c696703db48078083ee08ba527) snuck in and went unnoticed.

src/decoding/decoder.rs
@@ -115,7 +115,7 @@ impl<'ser> Decoder<'ser> {
         }
 
         #[cfg(debug)]
-        let ival = istr::from_utf8(&self.source[self.offset..curpos])
+        let ival = str::from_utf8(&self.source[self.offset..curpos])
             .expect("We've already examined every byte in the string");
 
         #[cfg(not(debug))]